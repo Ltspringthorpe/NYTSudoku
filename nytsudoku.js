c00493ade595fa5a90e2a2eef7d72cddc11043c4
Leanne Springthorpe <lrteeter@gmail.com>
2025-03-04 13:02:19 -0500

    adding comments to code

--- nytsudoku.js
+++ nytsudoku.js
@@ -1,4 +1,6 @@
 var highlight = function(e) {
+
+    // Get the currently selected number
     if (e.target.ariaLabel) {
         var num = e.target.ariaLabel;
         e.target.classList.add('selected');
@@ -6,13 +8,18 @@ var highlight = function(e) {
         var num = document.querySelector('.su-cell.selected').ariaLabel;
     }
 
+    // Remove previous highlights
     document.querySelectorAll('.su-cell').forEach(function(cell) {
         cell.style.backgroundColor = '';
     });
+
+    // Find all other squares to highlight
     if (num !== 'empty') {
-        document.querySelectorAll('.su-cell:not(.selected)[aria-label="' + num + '"]').forEach(function(newHighlighted) {
-            newHighlighted.style.backgroundColor = 'lightblue';
-        });
+        document
+            .querySelectorAll('.su-cell:not(.selected)[aria-label="' + num + '"]')
+            .forEach(function(newHighlighted) {
+                newHighlighted.style.backgroundColor = 'lightblue'; // <-- Change to any color you like
+            });
     }
 }
 
